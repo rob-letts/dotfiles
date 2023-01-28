71bbc604bfba04da6e040655bf7c1e24795e9a2d
Rob Letts <108027101+rob-letts@users.noreply.github.com>
2023-01-28 17:15:47 +0000

    update and organise lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -5,9 +5,9 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lspconfig = require('lspconfig')
 
 lspconfig['html'].setup { capabilities = capabilities }
-lspconfig['cssls'].setup { capabilities = capabilities }
 lspconfig['emmet_ls'].setup { capabilities = capabilities }
-lspconfig['volar'].setup { capabilities = capabilities }
-lspconfig['eslint'].setup { capabilities = capabilities }
-lspconfig['jsonls'].setup { capabilities = capabilities }
+lspconfig['cssls'].setup { capabilities = capabilities }
 lspconfig['tsserver'].setup { capabilities = capabilities }
+lspconfig['eslint'].setup { capabilities = capabilities }
+lspconfig['volar'].setup { capabilities = capabilities }
+lspconfig['jsonls'].setup { capabilities = capabilities }
