aa0f0544ec2edd8ce4d931e32b98be36d7570b39
Rob Letts <108027101+rob-letts@users.noreply.github.com>
2023-06-07 11:53:43 +0200

    corrections

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -13,6 +13,7 @@ lspconfig['cssls'].setup {  capabilities = capabilities }
 lspconfig['eslint'].setup {  capabilities = capabilities }
 lspconfig['volar'].setup { capabilities = capabilities }
 lspconfig['jsonls'].setup {  capabilities = capabilities }
+lspconfig['rust_analyzer'].setup {  capabilities = capabilities }
 
 lspconfig.denols.setup {
   capabilities = capabilities,
