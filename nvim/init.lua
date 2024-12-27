a9d2db8248e1fb23c28315961c4894c0e94453e5
Rob Letts <108027101+rob-letts@users.noreply.github.com>
2024-12-27 11:08:02 +0000

    fix: repair missing import

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,7 @@ require('remap')
 require('plugins')
 --
 require('plugins.treesitter')
+require('plugins.lsp')
 require('plugins.auto-complete')
 require('plugins.telescope')
 require('plugins.utilities')
