30b436ac9deb5f2818f8d00cf7e82debc17892ae
Rob Letts <108027101+rob-letts@users.noreply.github.com>
2024-12-27 11:08:28 +0000

    chore: tidy up lsp installs

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,5 +1,6 @@
 require('mason').setup()
 require('mason-lspconfig').setup()
+
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lspconfig = require('lspconfig')
 
@@ -9,12 +10,12 @@ vim.g.markdown_fenced_languages = {
 
 lspconfig.html.setup { capabilities = capabilities }
 lspconfig.emmet_ls.setup { capabilities = capabilities }
-lspconfig.astro.setup { capabilities = capabilities }
 lspconfig.cssls.setup { capabilities = capabilities }
 lspconfig.volar.setup { capabilities = capabilities }
 lspconfig.eslint.setup { capabilities = capabilities }
 lspconfig.jsonls.setup { capabilities = capabilities }
-lspconfig.zls.setup { capabilities = capabilities }
+lspconfig.vuels.setup { capabilities = capabilities }
+lspconfig.ts_ls.setup { capabilities = capabilities }
 
 lspconfig.lua_ls.setup {
   settings = {
@@ -24,23 +25,4 @@ lspconfig.lua_ls.setup {
       }
     }
   }
-}
-
-
-lspconfig.denols.setup {
-  capabilities = capabilities,
-  root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
-  settings = {
-    deno = {
-      enable = true,
-      unstable = true,
-      lint = true
-    }
-  }
-}
-
-lspconfig.tsserver.setup {
-  capabilities = capabilities,
-  root_dir = lspconfig.util.root_pattern("package.json"),
-  single_file_support = false
 }
